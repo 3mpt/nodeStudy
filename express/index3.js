d4c617e62e4c90b0e65e29e0fae3886ea4a14b6e
zhangyucong <zhangyucong@duiba.com.cn>
2024-06-14 10:37:51 +0800

    feat: 获取请求参数

--- express/index3.js
+++ express/index3.js
@@ -2,6 +2,9 @@ const express = require("express")
 const HomeRouter = require("./route3/HomeRouter")
 const LoginRouter = require("./route3/LoginRouter")
 const app = express()
+// 配置解析post参数中间件-不用下载第三方
+app.use(express.urlencoded({extended:false})) // post参数获取 username=kerwin&password=1234
+app.use(express.json())
 // 应用级别
 app.use(function (req, res, next) {
     console.log("验证token")
--- express/route3/LoginRouter.js
+++ express/route3/LoginRouter.js
@@ -1,7 +1,13 @@
 const express = require("express")
 const router = express.Router()
 //路由级别
-router.get("/",(req,res)=>{
-    res.send("login")
+router.get("/", (req, res) => {
+    console.log(req.query)
+    res.send("login-success")
+})
+
+router.post("/", (req, res) => {
+    console.log(req.body)
+    res.send({ ok: 1 })
 })
 module.exports = router
