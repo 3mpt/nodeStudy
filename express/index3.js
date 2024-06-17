a71c58aba02311c01fe9220621ca934bd212cbcf
zhangyucong <zhangyucong@duiba.com.cn>
2024-06-17 17:54:51 +0800

    feat: express

--- express/index3.js
+++ express/index3.js
@@ -3,11 +3,14 @@ const HomeRouter = require("./route3/HomeRouter")
 const LoginRouter = require("./route3/LoginRouter")
 const app = express()
 
+app.set("views", "./views")
+app.set("view engine", "html")
+app.engine("html",require("ejs").renderFile) // 支持直接渲染html文件
 // 配置静态资源
 app.use(express.static("public"))
 
 // 配置解析post参数中间件-不用下载第三方
-app.use(express.urlencoded({extended:false})) // post参数获取 username=kerwin&password=1234
+app.use(express.urlencoded({ extended: false })) // post参数获取 username=kerwin&password=1234
 app.use(express.json())
 // 应用级别
 app.use(function (req, res, next) {
@@ -18,7 +21,7 @@ app.use(function (req, res, next) {
 app.use("/home", HomeRouter)
 app.use("/login", LoginRouter)
 // 错误中间件
-app.use((req,res)=>{
+app.use((req, res) => {
     res.status(404).send('丢了')
 })
 app.listen(3000, () => {
