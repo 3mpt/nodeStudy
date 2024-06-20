b1c62e3c0e4cb107acadeb4a307e94b7e2f82735
zhangyucong <zhangyucong@duiba.com.cn>
2024-06-20 16:52:02 +0800

    feat: 文件上传1

--- 文件上传/app.js
+++ 文件上传/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
-var session = require("express-session")
+var uploadRouter = require('./routes/upload')
 var JWT = require("./util/JWT")
 var app = express();
 
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/api', usersRouter);
 app.use('/login', loginRouter);
-
+app.use('/upload',uploadRouter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- 文件上传/routes/upload.js
+++ 文件上传/routes/upload.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('upload', { title: 'Express' });
 });
 
 module.exports = router;
