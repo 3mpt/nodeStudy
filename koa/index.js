b340e97d1a9bff3d86625986e345d7d302e59d2d
zhangyucong <zhangyucong@duiba.com.cn>
2024-06-21 14:16:52 +0800

    feat: 路由1

--- koa/index.js
+++ koa/index.js
@@ -1,8 +1,32 @@
 const Koa = require("koa")
+const Router = require("koa-router")
 const app = new Koa()
-// ctx===context 上下文
-app.use((ctx, next) => {
-    ctx.response.body = "<b>hello world</b>"
+const router = new Router()
+//  增
+router.post('/list', (ctx, next) => {
+    ctx.body = {
+        ok: 1,
+        info: "添加成功"
+    }
 })
-app.listen(3000)
+// 获取
+router.get('/list', (ctx, next) => {
+    ctx.body = ['111', '122', '333']
+})
+// 修改
+router.put('/list/:id', (ctx, next) => {
+    console.log(ctx.params);
+    ctx.body = {
+        ok: 1,
+        info: "修改成功"
+    }
+})
+router.del('/list/:id', (ctx, next) => {
+    ctx.body = {
+        ok: 1,
+        info: "删除成功"
+    }
+})
+app.use(router.routes()).use(router.allowedMethods) // 404=>405 请求方式不对
 
+app.listen(3000)
