06009d3bd6278994c09a2adfd38834e41a2f79fb
zhangyucong <zhangyucong@duiba.com.cn>
2024-06-26 16:51:09 +0800

    feat: ws模块2

--- socket/server/app.js
+++ socket/server/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var chatRouter = require('./routes/chat');
 var session = require("express-session")
 const MongoStore = require("connect-mongo")
 var JWT = require("./util/JWT")
@@ -49,6 +50,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/api', usersRouter);
 app.use('/login', loginRouter);
+app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
