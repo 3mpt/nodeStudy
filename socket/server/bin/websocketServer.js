d97597f186c4ae057f3e7d77ff9ea5d16b5c7951
zhangyucong <zhangyucong@duiba.com.cn>
2024-06-27 11:10:32 +0800

    feat: ws模块3

--- socket/server/bin/websocketServer.js
+++ socket/server/bin/websocketServer.js
@@ -14,20 +14,57 @@ wss.on('connection', function connection(ws, req) {
     const payload = JWT.verify(myURL.searchParams.get("token"))
     if (payload) {
         ws.send(createMessage(WebSocketType.GroupChat, null, "欢迎来到聊天室"));
+        ws.user = payload
+        // 群发
+        sendAll()
     } else {
         ws.send(createMessage(WebSocketType.Error, null, "token过期"));
     }
     ws.on('error', console.error);
 
     ws.on('message', function message(data) {
-        console.log('received: %s', data);
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data, { binary: false });
-            }
-        });
-    });
+        // console.log('received: %s', data);
+        // wss.clients.forEach(function each(client) {
+        //     if (client !== ws && client.readyState === WebSocket.OPEN) {
+        //         client.send(data, { binary: false });
+        //     }
+        // });
+        const msgObj = JSON.parse(data)
+        switch (msgObj.type) {
+            case WebSocketType.GroupList:
+                console.log('@@@', Array.from(wss.clients).map(item =>
+                    item.user
+                ));
 
+                // console.log("@@@",wss.clients);
+                break;
+            case WebSocketType.GroupChat:
+                console.log(msgObj.data);
+                // 转发给其他人
+                wss.clients.forEach(function each(client) {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(
+                            createMessage(WebSocketType.GroupChat, ws.user, msgObj.data),
+                            { binary: false });
+                    }
+                });
+                break;
+            case WebSocketType.SingleChat:
+                wss.clients.forEach(function each(client) {
+                    if (client.user.username===msgObj.to&&client.readyState === WebSocket.OPEN) {
+                        client.send(
+                            createMessage(WebSocketType.SingleChat, ws.user, msgObj.data),
+                            { binary: false });
+                    }
+                });
+                break;
+
+
+        }
+    });
+    ws.on("close", () => {
+        console.log(ws.user);
+    })
 
 });
 const WebSocketType = {
@@ -42,4 +79,16 @@ function createMessage(type, user, data) {
         user,
         data
     })
+}
+function sendAll() {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(createMessage(WebSocketType.GroupList, null,
+                JSON.stringify(Array.from(wss.clients).map(item =>
+                    item.user
+                ))
+            ))
+        }
+    });
+
 }
